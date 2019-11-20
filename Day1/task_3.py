519d95436a77d025dfe6bd97e63e82a874ecc09b
Eredarion <ranguel95@gmail.com>
2019-11-21 01:16:13 +0200

    Refactor task 3

--- Day1/task_3.py
+++ Day1/task_3.py
@@ -1,11 +1,13 @@
-import re
+import bs4
 
 # -- Open and get text from file --
 html_file = open("./Day1/index.html", "r")
-html = html_file.read()
+html = bs4.BeautifulSoup(html_file.read(), "html.parser")
 html_file.close()
 
-# -- Find text -- 
-text = "My super text"
-list_1 = re.findall(text, html)
-print(list1)
+# -- Find text --
+p = html.find("p").getText()
+title = html.find('title').getText()
+
+print(title)
+print(p)
